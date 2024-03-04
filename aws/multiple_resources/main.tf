ab60f07c8e54df1fde12c652cc6bbe92a2803b4f
Sohit <sohit@digitalgreen.org>
2024-03-04 17:57:55 +0530

    (add) : added tf file for creating multiple resource

--- aws/multiple_resources/main.tf
+++ aws/multiple_resources/main.tf
@@ -10,15 +10,16 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-2"
-  # access_key = "ACCESS_KEY"
-  # secret_key = "SECRET_KEY"
+  access_key = "AKIAQ6FDGR5YY3TVUYLW" 
+  secret_key = "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"
 }
 
 resource "aws_instance" "app_server" {
+  count         = 3  # Creates 3 instances
   ami           = "ami-0f5daaa3a7fb3378b"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Terraform_Demo"
+    Name = "Terraform_Demo-${count.index}"
   }
 }

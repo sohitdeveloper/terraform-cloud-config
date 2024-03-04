52731b9eb1b60bdec27bb6c8e302c2e6d5b1e9bc
Sohit Kumar <79898903+sohitdeveloper@users.noreply.github.com>
2024-03-04 17:31:00 +0530

    (init) : infra creation for aws

---
+++ aws/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"
+  access_key = "ACCESS_KEY"
+  secret_key = "SECRET_KEY"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0f5daaa3a7fb3378b"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform_Demo"
+  }
+}

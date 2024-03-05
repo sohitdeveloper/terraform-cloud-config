fea9fb9cf41289f8526b4b50f821136623bc5c10
Sohit <sohit@digitalgreen.org>
2024-03-05 13:12:37 +0530

    (add) : ssh accessibilites added, port opening

--- aws/main.tf
+++ aws/main.tf
@@ -9,15 +9,40 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-2"
-  # access_key = "ACCESS_KEY"
-  # secret_key = "SECRET_KEY"
+  region = "us-east-1"
+  # access_key = "access_key"
+  # secret_key = "secret_key"
+}
+
+# Security Group to allow SSH
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Replace with your IP address or range
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0f5daaa3a7fb3378b"
+  ami           = "ami-07d9b9ddc6cd8dd30"
   instance_type = "t2.micro"
-
+  key_name      = "ubuntu-key" # Replace with your AWS key pair name
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   tags = {
     Name = "Terraform_Demo"
   }
